67515020b82f6ab054d2380ec7db72172319e9ab
Colin Murphy <dietcoke3.14@gmail.com>
2013-10-07 21:28:46 -0400

    First Commit

---
+++ sigFig.py
@@ -0,0 +1,24 @@
+numb=str(raw_input())
+numb = numb+"t"
+print numb
+sigfig=0
+zeroCounter=0
+for i in numb:
+    if i!="0" and i!="t" and i!=".":
+        sigfig+=1
+
+        if zeroCounter >=1:
+            sigfig+=zeroCounter
+            zeroCounter=0
+            
+        
+    elif i=="0":
+        zeroCounter +=1
+    elif i=="t":
+        sigfig+=zeroCounter
+        zeroCounter=0
+
+print sigfig
+
+
+
